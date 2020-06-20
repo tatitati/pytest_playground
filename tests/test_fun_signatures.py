0daca2a3c7aa13d3a924c381c550ecfc63f6b2ba
tatitati <francis.albusac@simplybusiness.co.uk>
2020-06-20 16:21:02 +0100

    more signatures experiments

--- tests/test_fun_signatures.py
+++ tests/test_fun_signatures.py
@@ -6,10 +6,16 @@ import source.calc as calc
 def fun1(name: str, age: int) -> str:    
     return name + str(age)
 
+def fun2(name: str, age) -> str:    
+    return name + str(age)    
+
 
 class TestCalc(unittest.TestCase):
 
     def test_basic(self):
-        result = fun1("whatever", 5)
-        self.assertEqual(result, "whatever5", "should be 15")
+        result1 = fun1("whatever", 5)
+        result2 = fun2("whatever", 5)
+        
+        self.assertEqual(result1, "whatever5", "should be 15")
+        self.assertEqual(result2, "whatever5", "should be 15")
         
