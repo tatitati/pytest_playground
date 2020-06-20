962d8255aa8fa555660a68b756caf0dbd58638ac
tatitati <francis.albusac@simplybusiness.co.uk>
2020-06-20 15:48:23 +0100

    argv tested

--- tests/test_argv.py
+++ tests/test_argv.py
@@ -2,17 +2,28 @@ import unittest
 from functools import partial
 import source.calc as calc
 
-def myFun(*params):  
+def myFun1(*params):  
     initialText=""
     for arg in params:  
         initialText += arg + "!!"
 
     return initialText
     
+def myFun2(name, *params):  
+    initialText=name
+    for arg in params:  
+        initialText += arg + "!!"
+
+    return initialText
+
 
 class TestArgv(unittest.TestCase):
     
-    def test_basic(self):
-        result = myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')  
+    def test_basic1(self):
+        result = myFun1('Hello', 'Welcome', 'to', 'GeeksforGeeks')  
         self.assertEqual(result, "Hello!!Welcome!!to!!GeeksforGeeks!!")
+
+    def test_basic2(self):
+        result = myFun2('Francisco', 'Hello', 'Welcome', 'to', 'GeeksforGeeks')  
+        self.assertEqual(result, "FranciscoHello!!Welcome!!to!!GeeksforGeeks!!")
         
---
+++ tests/test_kwargs.py
@@ -0,0 +1,18 @@
+import unittest
+from functools import partial
+import source.calc as calc
+
+def myFun(*params):  
+    initialText=""
+    for arg in params:  
+        initialText += arg + "!!"
+
+    return initialText
+    
+
+class TestKwargs(unittest.TestCase):
+    
+    def test_basic(self):
+        result = myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')  
+        self.assertEqual(result, "Hello!!Welcome!!to!!GeeksforGeeks!!")
+        
