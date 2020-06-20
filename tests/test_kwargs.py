03c3094cf1d824c5784e5bd2de3563718e9bca28
tatitati <francis.albusac@simplybusiness.co.uk>
2020-06-20 15:53:20 +0100

    added tests for kwargs

--- tests/test_kwargs.py
+++ tests/test_kwargs.py
@@ -2,17 +2,29 @@ import unittest
 from functools import partial
 import source.calc as calc
 
-def myFun(*params):  
-    initialText=""
-    for arg in params:  
-        initialText += arg + "!!"
+def myFun1(**kwargs):  
+    initialText = ""
+    for key, value in kwargs.items(): 
+        initialText += key + "=>" +  value
+
+    return initialText
+
+def myFun2(name, **kwargs):  
+    initialText = name
+    for key, value in kwargs.items(): 
+        initialText += key + "=>" +  value
 
     return initialText
     
 
 class TestKwargs(unittest.TestCase):
     
-    def test_basic(self):
-        result = myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')  
-        self.assertEqual(result, "Hello!!Welcome!!to!!GeeksforGeeks!!")
+    def test_basic1(self):
+        result = myFun1(first ='Geeks', mid ='for', last='Geeks')          
+        self.assertEqual(result, "first=>Geeksmid=>forlast=>Geeks")
+
+
+    def test_basic2(self):
+        result = myFun2('francisco', first ='Geeks', mid ='for', last='Geeks')          
+        self.assertEqual(result, "franciscofirst=>Geeksmid=>forlast=>Geeks")
         
